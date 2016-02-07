996ddc81a4552dfaf31fb58e1e525573de29c709
Nellcorp <figassis@nellcorp.com>
2016-02-07 04:23:03 -0500

    building API

--- node/app.js
+++ node/app.js
@@ -5,13 +5,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var Mongoose = require('mongoose');
-var db = Mongoose.createConnection('localhost', 'node');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/node', function(err) {
+    if(err) {
+        console.log('connection error', err);
+    } else {
+        console.log('connection successful');
+    }
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var todos = require('./routes/todos');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/todos', todos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
